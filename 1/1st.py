2bd0ecb1932246e0193a4e00ed6ba0e78f2a8524
Duc Nguyen <nguyenmanhduc1309@gmail.com>
2021-02-19 16:11:46 +0000

    Update 1st.py

--- 1/1st.py
+++ 1/1st.py
@@ -1,7 +1,11 @@
 f = open('input.txt', 'r')
 content = f.read()
 x = content.split()
+
+# How many numbers in the file:
 length = len(x)
+
+
 for i in range(length):
     first = int(x[i])
     for j in range(i, length):
